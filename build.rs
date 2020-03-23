c819a7bc1a4ab7e4965d23afc69e7f4c8ab18b2a
Fabio Lagalla <lagfabio@amazon.com>
2020-03-23 18:00:52 +0100

    Build.rs: add header and comments

--- build.rs
+++ build.rs
@@ -1,3 +1,18 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify,
+// merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 use std::env;
 use std::fs;
 use std::path::Path;
@@ -21,6 +36,9 @@ fn cpuid(functionnumber: u32, output: &mut [u32; 4]) {
 }
 
 fn main() {
+    // This build script generates the cargo build config file (.cargo/config)
+    // The rust flags are set in order to avoid generating illegal instructions
+    // on the machine on which the build is triggered.
     let mut target_features: Vec<&str> = Vec::new();
 
     let features = &mut [0u32; 4];
