591fda1fc7b04497be47099d60fbc41cea023400
Fabio Lagalla <lagfabio@amazon.com>
2021-02-24 17:10:18 +0100

    lib: Make the C api public

--- src/lib.rs
+++ src/lib.rs
@@ -904,7 +904,7 @@ pub fn convert_image(
 
 #[doc(hidden)]
 #[cfg(not(feature = "test_instruction_sets"))]
-mod c_bindings {
+pub mod c_api {
     #![allow(clippy::wildcard_imports)]
     use super::*; // We are importing everything
     use pixel_format::{are_planes_compatible, MAX_NUMBER_OF_PLANES};
