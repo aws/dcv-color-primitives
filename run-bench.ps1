76617203b6ccdb08d35cd819c53caf20d0b56dad
Fabio Lagalla <lagfabio@amazon.com>
2026-01-12 09:39:59 +0100

    Support no-std build

--- run-bench.ps1
+++ run-bench.ps1
@@ -14,7 +14,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-param([String]$filter="") 
+param([String]$filter="")
 
 [Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
 [Console]::TreatControlCAsInput = $true
